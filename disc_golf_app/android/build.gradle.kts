0fc39afeaa65391878d5833de9d2af32dc0ed5de
Nicholas <barrn06@yahoo.com>
2026-05-31 20:45:17 -0500

    Fix Android JVM target compatibility

--- disc_golf_app/android/build.gradle.kts
+++ disc_golf_app/android/build.gradle.kts
@@ -1,3 +1,7 @@
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+
 allprojects {
     repositories {
         google()
@@ -17,6 +21,38 @@ subprojects {
 }
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    plugins.withId("com.android.application") {
+        extensions.configure<ApplicationExtension>("android") {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+
+    plugins.withId("com.android.library") {
+        extensions.configure<LibraryExtension>("android") {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+
+    plugins.withId("kotlin-android") {
+        extensions.configure<KotlinAndroidProjectExtension>("kotlin") {
+            jvmToolchain(17)
+        }
+    }
+
+    plugins.withId("org.jetbrains.kotlin.android") {
+        extensions.configure<KotlinAndroidProjectExtension>("kotlin") {
+            jvmToolchain(17)
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
