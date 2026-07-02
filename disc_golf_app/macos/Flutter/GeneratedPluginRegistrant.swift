68f6fd819a28bdddd575dfbb230d0f83fe19ebe6
Nicholas <barrn06@yahoo.com>
2026-07-02 12:26:32 +0000

    feat: add flutter_secure_storage plugin support across platforms

--- disc_golf_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ disc_golf_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import ffmpeg_kit_flutter_new
 import file_picker
 import file_selector_macos
+import flutter_secure_storage_macos
 import gal
 import share_plus
 import shared_preferences_foundation
@@ -17,6 +18,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
